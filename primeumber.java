580fce425c2fa595919b30398522795fd0b47126
Lalith Kishore V <100907116+LalithKishore2002@users.noreply.github.com>
2023-11-06 12:17:00 +0530

    Update primeumber.java

--- primeumber.java
+++ primeumber.java
@@ -1,19 +1,25 @@
-public class hello{    
- public static void main(String args[]){    
-  int i,m=0,p=0;      
-  int n=3;   
-  m=n/2;      
-  if(n==0||n==1){  
-   System.out.println(n+" not prime no");      
-  }else{  
-   for(i=2;i<=m;i++){      
-    if(n%i==0){      
-     System.out.println(n+" not prime no");      
-     p=1;      
-     break;      
-    }      
-   }      
-   if(p==0)  { System.out.println(n+"  prime no"); }  
-  }
-}    
-}   
+public class practice {
+public static void main(String[] args) {
+    int i,m=0,p=0;
+    int n=18;
+    m=n/2;
+    if (n==0||n==1) {
+        System.out.println(n+" not prime");
+
+    }else{
+        for(i=2;i<=m;i++)
+        {
+            if (n%i==0) {
+                System.out.println(n+ " not prime");
+                 p=1;
+                 break;           
+        }
+    }
+    if (p==0) {
+        System.out.println(n+" is prime");
+    }
+    }
+
+}
+    
+}
